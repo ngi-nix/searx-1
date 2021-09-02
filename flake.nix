6b892735d4f8348ab2fa5dc450eaa0e855e2f773
efim <efim.wool@gmail.com>
2021-09-02 14:55:20 +0300

    nix flake cleanup & documentation

--- flake.nix
+++ flake.nix
@@ -10,26 +10,20 @@
 
   outputs = { self, nixpkgs }:
     let
-
       # System types to support.
       supportedSystems = [ "x86_64-linux" ];
-
       # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
       forAllSystems = f:
         nixpkgs.lib.genAttrs supportedSystems (system: f system);
-
       # Nixpkgs instantiated for supported system types.
       nixpkgsFor = forAllSystems (system:
         import nixpkgs {
           inherit system;
           overlays = [ self.overlay ];
         });
-
       in {
 
         overlay = final: prev: rec {
-
-          # TODO should this be in python packages?
           tgwf-green-results-searx-plugin = prev.python38Packages.buildPythonPackage {
             pname = "tgwf-searx-plugin";
             version = "0.2";
@@ -40,12 +34,10 @@
             doCheck = false;
           };
 
-          python38 = let
-            packageOverrides = python-self: python-super: {
-              tgwf-green-results-searx-plugin = final.tgwf-green-results-searx-plugin;
-            };
-          in prev.python38.override {inherit packageOverrides;};
-
+          # overlaying searx with version that depends plugin
+          # results in plugin being installed in the searx environment via python setuptools
+          # which is the requirement for the plugin to be available for searx:
+          # https://searx.github.io/searx/dev/plugins.html#external-plugins
           searx = prev.searx.overrideAttrs (oldAttrs: {
               propagatedBuildInputs = oldAttrs.propagatedBuildInputs ++ [
                 tgwf-green-results-searx-plugin
@@ -67,12 +59,12 @@
         # The default package for 'nix build'. This makes sense if the
         # flake provides only one package or there is a clear "main"
         # package.
+        # Returns Searx with plugin enabled
         defaultPackage = forAllSystems (system: self.packages.${system}.searx);
 
         # To use as `nix develop`
         # will provide shell with
         # - `searx-run`
-        # - python with installed plugin module
         # - simple searx config that would allow its launch & plugin utilization
         devShell = forAllSystems (system: nixpkgsFor.${system}.mkShell rec {
           packages = with nixpkgsFor.${system}; [ searx tgwf-green-results-searx-plugin ];
@@ -88,6 +80,22 @@
           SEARX_SETTINGS_PATH = "${settingsFile}";
         });
 
+        # Can be used to add plugin to searx installation
+        # in the server configuration
+        # 1. add this flake as input
+        # ```
+        # inputs = {
+        #   nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
+        #   tgwf-searx-plugin.url = "github:ngi-nix/searx-thegreenopenweb/nix-flake"; # TODO change to TGWF repo
+        # };
+        # ```
+        # 2. add module for import:
+        # ```
+        # modules = [ tgwf-searx-plugin.nixosModules.tgwf-green-results-searx-plugin-module ]
+        # ```
+        # This will add overlay for searx, to depend on plugin and have it installed in the python env
+        # and add configuration line to mark plugin for availability in settins
+        # see: https://searx.github.io/searx/dev/plugins.html#external-plugins
         nixosModules.tgwf-green-results-searx-plugin-module = { pkgs, ... }:
         {
           nixpkgs.overlays = [ self.overlay ];
